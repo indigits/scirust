92a67994f0ca592739a0fa9d4877170f6e3fcfe2
Shailesh Kumar <shailesh@indigits.com>
2014-10-24 21:55:05 +0530

    changes in the names of many structures methods.

--- doc/implementors/core/clone/trait.Clone.js
+++ doc/implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['srmat'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;T: MatElt&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='srmat/struct.Mat.html' title='srmat::Mat'>Mat</a>&lt;T&gt;",];
+implementors['srmat'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;T: MatrixElt&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='srmat/struct.Matrix.html' title='srmat::Matrix'>Matrix</a>&lt;T&gt;","<a class='stability Unmarked' title='No stability level'></a>impl&lt;T: MatrixElt&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='srmat/struct.Matrix.html' title='srmat::Matrix'>Matrix</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
