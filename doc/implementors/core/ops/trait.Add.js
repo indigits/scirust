824dced743a3008254fbc2667a262f26a85ca4a3
Shailesh Kumar <shailesh@indigits.com>
2014-10-24 11:41:43 +0530

    first version of documentation

---
+++ doc/implementors/core/ops/trait.Add.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['srmat'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;T: MatElt&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Add.html' title='core::ops::Add'>Add</a>&lt;<a class='struct' href='srmat/struct.Mat.html' title='srmat::Mat'>Mat</a>&lt;T&gt;, <a class='struct' href='srmat/struct.Mat.html' title='srmat::Mat'>Mat</a>&lt;T&gt;&gt; for <a class='struct' href='srmat/struct.Mat.html' title='srmat::Mat'>Mat</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/core/ops/trait.Index.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['srmat'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;T: MatElt&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Index.html' title='core::ops::Index'>Index</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.uint.html'>uint</a>, T&gt; for <a class='struct' href='srmat/struct.Mat.html' title='srmat::Mat'>Mat</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
