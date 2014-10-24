7f7ddbb24fc2c5252c71d40dd8b1cbc277444295
Shailesh Kumar <shailesh@indigits.com>
2014-10-24 19:30:38 +0530

    not a vector error defined.

--- src/materr.rs
+++ src/materr.rs
@@ -1,7 +1,12 @@
 /// Errors related to matrix operations
 #[deriving(Show)]
 pub enum MatErr{
+    /// The matrix is empty
     EmptyMatrix,
+    /// The dimensions of two matrices mismatch
     DimensionsMismatch,
+    /// The matrix is not a square matrix
     NonSquareMatrix,
+    /// The object is not a vector
+    NotAVector,
 }
