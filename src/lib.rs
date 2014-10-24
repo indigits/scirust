730841b3fa344876354d50a62ec7a014b85dd858
Shailesh Kumar <shailesh@indigits.com>
2014-10-24 23:52:15 +0530

    rand_std_normal function defined.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![feature(unsafe_destructor, globs)]
 pub use  self::matrix::*;
+pub use  self::matrand::*;
 mod discrete;
 mod matelt;
 mod materr;
 mod matrix;
+mod matrand;
---
+++ src/matrand.rs
@@ -0,0 +1,43 @@
+// library imports
+use std::rand;
+use std::rand::Rng;
+use std::rand::distributions::normal::{StandardNormal};
+use super::matrix::*;
+
+
+
+/// Generate a random matrix of uniformly distributed numbers
+pub fn rand_std_normal(rows: uint, cols : uint)-> MatrixF64 {
+    let mut m : Matrix<f64> = Matrix::new(rows, cols);
+    let mut rng = rand::task_rng();
+    for c in range(0, cols){
+        for r in range (0, rows){
+            let StandardNormal(n) = rng.gen::<StandardNormal>();
+            m.set(r, c, n);
+        }
+    }
+    m
+}
+
+
+
+/******************************************************
+ *
+ *   Unit tests follow.
+ *
+ *******************************************************/
+
+
+#[cfg(test)]
+#[allow(unused_imports)]
+mod tests {
+
+    use matrix::{Matrix, MatrixF64};
+
+    #[test]
+    fn test_rand_std_normal(){
+        let m1  = super::rand_std_normal(10, 10);
+        assert_eq!(m1.num_cells(), 100);
+    }
+
+}
