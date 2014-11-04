969f7ea794c8b0c0bd16e48dd8d8d9c6e325280f
Shailesh Kumar <shailesh@indigits.com>
2014-11-04 13:01:46 +0530

    created a separate directory for discrete math related stuff.

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,21 @@ pub use  self::matrand::*;
 pub use  self::matiter::*;
 pub use  self::matview::*;
 
-mod discrete;
 mod matelt;
 mod materr;
 mod matrix;
 mod matrand;
 mod matiter;
 mod matview;
+
+
+pub mod discrete{
+
+    pub use self::modular::*;
+
+    mod modular;
+
+}
 
 pub mod linalg {
     pub use self::gauss_elim::*;
