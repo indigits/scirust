1a00f3514f2dc74964e6e058500859c797814a2b
Shailesh Kumar <shailesh@indigits.com>
2014-11-05 22:18:49 +0530

    functions for creating a matrix from a range of numbers.

--- src/matrix/special.rs
+++ src/matrix/special.rs
@@ -6,8 +6,9 @@ use std::num;
 
 
 // local imports
-use matrix::matrix::{Matrix, MatrixF64};
+use matrix::matrix::{Matrix, MatrixI64, MatrixF64};
 use matrix::error::*;
+use matrix::element::MatrixElt;
 
 #[doc="Returns a hadamard matrix of size n x n
 
@@ -46,9 +47,60 @@ pub fn hadamard(n : uint) -> Result<MatrixF64, MatrixError>{
 }
 
 
+#[doc="Returns a matrix whose entries are picked up from
+a range in column wise order.
+
+# Remarks
+
+Say you wish to create a matrix of 100 elements. If you
+provide a range of 80 numbers, the first 80 entries in the
+matrix will be filled by the numbers from the range. The
+remaining 20 entries will be filled with zeros. On the
+other hand, if you provide a range with more than 100 numbers,
+then only the first 100 numbers will be used to fill the matrix
+(off course in column major order). The remaining numbers
+in the range will not be used. They will also not be generated.
+
+
+# Examples
+
+Constructing a 4x4 matrix of floating point numbers:
+
+        use scirust::matrix::MatrixF64;
+        use scirust::matrix::special::from_range;
+        let start  = 0.0;
+        let stop = 16.0;
+        let m : MatrixF64 = from_range(4, 4, start, stop);
+        for i in range(0, 16){
+            let c = i >> 2;
+            let r = i & 3;
+            assert_eq!(m.get(r, c), i as f64);
+        }
+
+
+"]
+pub fn from_range<T:MatrixElt+PartialOrd+Clone+ToPrimitive>(rows : uint, cols : uint, 
+    start : T, stop : T )-> Matrix<T> {
+    let m : Matrix<T> = Matrix::from_iter(rows, cols, range(start, stop));
+    m 
+}
+
+#[doc="Returns a 64-bit integer matrix whose entries are picked up from
+a range in column wise order.
+
+See from_range function  for further discussion.
+
+"]pub fn from_range_i64(rows : uint, cols : uint, 
+    start : i64, stop : i64)->MatrixI64 {
+    let m : MatrixI64 = Matrix::from_iter(rows, cols, range(start, stop));
+    m 
+}
+
+
 #[cfg(test)]
 mod test{
     use super::*;
+    use matrix::matrix::*;
 
     #[test]
     fn test_hadamard(){
@@ -63,5 +115,23 @@ mod test{
         assert_eq!(m.get(1,1), -1.0);
         let m = hadamard(4).unwrap();
         assert!(m.is_square());
+    }
+
+    #[test]
+    fn test_range(){
+        let m = from_range_i64(4, 4, 0, 16);
+        for i in range(0, 16){
+            let c = i >> 2;
+            let r = i & 3;
+            assert_eq!(m.get(r, c), i as i64);
+        }
+        let start  = 0.0;
+        let stop = 16.0;
+        let m : MatrixF64 = from_range(4, 4, start, stop);
+        for i in range(0, 16){
+            let c = i >> 2;
+            let r = i & 3;
+            assert_eq!(m.get(r, c), i as f64);
+        }
     }
 }
