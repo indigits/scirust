ceb4f64d113b9b60aaa969e23d3c60a22ff284d7
Shailesh Kumar <shailesh@indigits.com>
2014-11-10 21:14:44 +0530

    doc updates.

---
+++ doc/implementors/collections/hash/trait.Hash.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['scirust'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/nightly/collections/hash/trait.Hash.html' title='collections::hash::Hash'>Hash</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/collections/hash/trait.Hash.html' title='collections::hash::Hash'>Hash</a> for <a class='struct' href='scirust/external/complex/struct.Complex.html' title='scirust::external::complex::Complex'>Complex</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
