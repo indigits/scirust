ade0722e7fd33871f8697ad5e2b3eaf3d59407e5
Shailesh Kumar <shailesh@indigits.com>
2014-11-14 12:50:30 +0530

    Some introspection capabilities

--- src/number.rs
+++ src/number.rs
@@ -14,52 +14,158 @@ use external::complex::{Complex32, Complex64};
 /// Defines all the traits which a matrix element must support
 pub trait Number : Num+Copy+Show {
 
+    fn is_float() -> bool {
+        false
+    }
+
+    fn is_int() -> bool {
+        false
+    }
+
+    fn is_complex() -> bool {
+        false
+    }
+
+    fn is_signed() -> bool;
 }
 
 /// Indicate that i8 fits all requirements for being a matrix element.
 impl Number for i8 {
+
+    #[inline]
+    fn is_signed() -> bool {
+        true
+    }
     
 }
 /// Indicate that i16 fits all requirements for being a matrix element.
 impl Number for i16 {
+
+    #[inline]
+    fn is_signed() -> bool {
+        true
+    }
     
+    fn is_int() -> bool {
+        true
+    }
+
 }
 /// Indicate that i32 fits all requirements for being a matrix element.
 impl Number for i32 {
     
+    #[inline]
+    fn is_signed() -> bool {
+        true
+    }
+
+    fn is_int() -> bool {
+        true
+    }
+
 }
 /// Indicate that i64 fits all requirements for being a matrix element.
 impl Number for i64 {
     
+    #[inline]
+    fn is_signed() -> bool {
+        true
+    }
+
+    
+    fn is_int() -> bool {
+        true
+    }
+
 }
 
 /// Indicate that int fits all requirements for being a matrix element.
 impl Number for int {
     
+    
+    #[inline]
+    fn is_signed() -> bool {
+        true
+    }
+
+    fn is_int() -> bool {
+        true
+    }
+
 }
 
 
 /// Indicate that u8 fits all requirements for being a matrix element.
 impl Number for u8 {
     
+    
+    #[inline]
+    fn is_signed() -> bool {
+        false
+    }
+
+    fn is_int() -> bool {
+        true
+    }
+
 }
 /// Indicate that u16 fits all requirements for being a matrix element.
 impl Number for u16 {
     
+    
+    #[inline]
+    fn is_signed() -> bool {
+        false
+    }
+
+    fn is_int() -> bool {
+        true
+    }
+
 }
 /// Indicate that u32 fits all requirements for being a matrix element.
 impl Number for u32 {
     
+    
+    #[inline]
+    fn is_signed() -> bool {
+        false
+    }
+
+    fn is_int() -> bool {
+        true
+    }
+
 }
 
 /// Indicate that u64 fits all requirements for being a matrix element.
 impl Number for u64 {
     
+    
+    #[inline]
+    fn is_signed() -> bool {
+        false
+    }
+
+    fn is_int() -> bool {
+        true
+    }
+
 }
 
 /// Indicate that uint fits all requirements for being a matrix element.
 impl Number for uint {
     
+    
+    #[inline]
+    fn is_signed() -> bool {
+        false
+    }
+
+    fn is_int() -> bool {
+        true
+    }
+
 }
 
 
@@ -67,22 +173,60 @@ impl Number for uint {
 /// Indicate that f32 fits all requirements for being a matrix element.
 impl Number for f32 {
     
+    
+    #[inline]
+    fn is_signed() -> bool {
+        true
+    }
+
+    fn is_float() -> bool {
+        true
+    }
+
 }
 
 
 /// Indicate that f64 fits all requirements for being a matrix element.
 impl Number for f64 {
     
+    
+    #[inline]
+    fn is_signed() -> bool {
+        true
+    }
+
+    fn is_float() -> bool {
+        true
+    }
+
 }
 
 
 /// Indicate that Complex32 fits all requirements for being a matrix element.
 impl Number for Complex32 {
     
+    
+    #[inline]
+    fn is_signed() -> bool {
+        true
+    }
+
 }
 
 
 /// Indicate that Complex64 fits all requirements for being a matrix element.
 impl Number for Complex64 {
     
+    
+    #[inline]
+    fn is_signed() -> bool {
+        true
+    }
+
+}
+
+
+//TODO: figure out how to make this work.
+pub fn describe<T: Number>(value : T ){
+    //println!("Signed: {}", T.is_signed());
 }
