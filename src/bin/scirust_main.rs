c841365288b7ca12d36a1d86727b9f94a0ff31ed
Shailesh Kumar <shailesh@indigits.com>
2014-11-17 15:51:53 +0530

    Dummy main application.

---
+++ src/bin/scirust_main.rs
@@ -0,0 +1,17 @@
+#![feature(globs)]
+extern crate scirust;
+#[cfg(not(test))]
+fn main(){
+    use scirust::matrix::*;
+    use scirust::linalg::*;
+    let a = matrix_cw_f64(2,2, [1., 4., 2., 8.]);
+    println!("{}", a);
+    let b = vector_f64([3.0, 6.0]);
+    let result = GaussElimination::new(&a, &b).solve();
+    match result {
+        Ok(x) => println!("{}", x),
+        Err(e) => println!("{}", e),
+    }
+    let m = matrix_cw_c64(2,2, []);
+    println!("{}", m);
+}
