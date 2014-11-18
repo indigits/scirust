c21eb9508814e5c5186ab8eacdb667c026ec2726
Shailesh Kumar <shailesh@indigits.com>
2014-11-18 13:19:19 +0530

    Bug in code with changed API.

--- src/bin/scirust_main.rs
+++ src/bin/scirust_main.rs
@@ -2,8 +2,7 @@
 extern crate scirust;
 #[cfg(not(test))]
 fn main(){
-    use scirust::matrix::*;
-    use scirust::linalg::*;
+    use scirust::api::*;
     let a = matrix_cw_f64(2,2, [1., 4., 2., 8.]);
     println!("{}", a);
     let b = vector_f64([3.0, 6.0]);
