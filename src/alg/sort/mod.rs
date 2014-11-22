1b7e7e8d4d2c6e8e64d1e88a65b79e5eee14a1d7
Shailesh Kumar <shailesh@indigits.com>
2014-11-22 15:45:38 +0530

    introduced asc / desc checks on strided buffers.

--- src/alg/sort/mod.rs
+++ src/alg/sort/mod.rs
@@ -34,39 +34,75 @@ pub fn is_descending_slice<T : PartialOrd>(data : & [T]) -> bool {
 }
 
 /// Returns whether the data in buffer is sorted in ascending order or not.
-pub fn is_ascending_buffer<T : PartialOrd>(data : *const T, n : uint) -> bool {    
+pub unsafe fn is_ascending_buffer<T : PartialOrd>(data : *const T, n : uint) -> bool {    
     if n == 0 {
         return true;
     }
     let mut ptr = data;
     for _ in range(0, n-1){
-        unsafe {
-            if *ptr > *ptr.offset(1i){
-                return false;
-            }
-            ptr = ptr.offset(1i);
+        if *ptr > *ptr.offset(1i){
+            return false;
         }
+        ptr = ptr.offset(1i);
     }
     true
 }
 
 /// Returns whether the data in buffer is sorted in descending order or not.
-pub fn is_descending_buffer<T : PartialOrd>(data : *const T, n : uint) -> bool {    
+pub unsafe fn is_descending_buffer<T : PartialOrd>(data : *const T, n : uint) -> bool {    
     if n == 0 {
         return true;
     }
     let mut ptr = data;
     for _ in range(0, n-1){
-        unsafe {
-            if *ptr < *ptr.offset(1i){
-                return false;
-            }
-            ptr = ptr.offset(1i);
+        if *ptr < *ptr.offset(1i){
+            return false;
         }
+        ptr = ptr.offset(1i);
     }
     true
 }
 
+
+/// Returns whether the data in strided buffer is sorted in ascending order or not
+/// every stride-th entry is considered in the buffer
+/// assumes that the buffer as at least n* stride entries
+pub unsafe fn is_ascending_buffer_strided<T : PartialOrd>(data : *const T, 
+    n : uint, 
+    stride : uint) -> bool {    
+    if n == 0 {
+        return true;
+    }
+    let mut ptr = data;
+    let stride = stride as int;
+    for _ in range(0, n-1){
+        if *ptr > *ptr.offset(stride){
+            return false;
+        }
+        ptr = ptr.offset(stride);
+    }
+    true
+}
+
+/// Returns whether the data in strided buffer is sorted in ascending order or not
+/// every stride-th entry is considered in the buffer
+/// assumes that the buffer as at least n* stride entries
+pub unsafe fn is_descending_buffer_strided<T : PartialOrd>(data : *const T, 
+    n : uint, 
+    stride : uint) -> bool {    
+    if n == 0 {
+        return true;
+    }
+    let mut ptr = data;
+    let stride = stride as int;
+    for _ in range(0, n-1){
+        if *ptr < *ptr.offset(stride){
+            return false;
+        }
+        ptr = ptr.offset(stride);
+    }
+    true
+}
 
 
 /******************************************************
@@ -101,20 +137,22 @@ mod test{
 
     #[test]
     fn test_is_ascending_buffer(){
-        let v = vec!(1i, 2, 3, 4);
-        assert!(is_ascending_buffer(v.as_ptr(), v.len()));
-        let v = vec!(1i);
-        assert!(is_ascending_buffer(v.as_ptr(), v.len()));
-        let v : Vec<int> = vec!();
-        assert!(is_ascending_buffer(v.as_ptr(), v.len()));
-        let v = vec!(2i, 1);
-        assert!(!is_ascending_buffer(v.as_ptr(), v.len()));
-        let v = vec!(3i, 2, 1);
-        assert!(!is_ascending_buffer(v.as_ptr(), v.len()));
-        let v = vec!(1i, 3, 2);
-        assert!(!is_ascending_buffer(v.as_ptr(), v.len()));
-        let v = vec!(2i, 1, 3);
-        assert!(!is_ascending_buffer(v.as_ptr(), v.len()));
+        unsafe {
+            let v = vec!(1i, 2, 3, 4);
+            assert!(is_ascending_buffer(v.as_ptr(), v.len()));
+            let v = vec!(1i);
+            assert!(is_ascending_buffer(v.as_ptr(), v.len()));
+            let v : Vec<int> = vec!();
+            assert!(is_ascending_buffer(v.as_ptr(), v.len()));
+            let v = vec!(2i, 1);
+            assert!(!is_ascending_buffer(v.as_ptr(), v.len()));
+            let v = vec!(3i, 2, 1);
+            assert!(!is_ascending_buffer(v.as_ptr(), v.len()));
+            let v = vec!(1i, 3, 2);
+            assert!(!is_ascending_buffer(v.as_ptr(), v.len()));
+            let v = vec!(2i, 1, 3);
+            assert!(!is_ascending_buffer(v.as_ptr(), v.len()));
+        }
     }
 
     #[test]
@@ -138,22 +176,37 @@ mod test{
 
     #[test]
     fn test_is_descending_buffer(){
-        let v = vec!(4i, 3, 2, 1);
-        assert!(is_descending_buffer(v.as_ptr(), v.len()));
-        let v = vec!(1i);
-        assert!(is_descending_buffer(v.as_ptr(), v.len()));
-        let v : Vec<int> = vec!();
-        assert!(is_descending_buffer(v.as_ptr(), v.len()));
-        let v = vec!(1i, 2);
-        assert!(!is_descending_buffer(v.as_ptr(), v.len()));
-        let v = vec!(1i, 2, 3);
-        assert!(!is_descending_buffer(v.as_ptr(), v.len()));
-        let v = vec!(2i, 3, 1);
-        assert!(!is_descending_buffer(v.as_ptr(), v.len()));
-        let v = vec!(3i, 1, 2);
-        assert!(!is_descending_buffer(v.as_ptr(), v.len()));
+        unsafe {
+            let v = vec!(4i, 3, 2, 1);
+            assert!(is_descending_buffer(v.as_ptr(), v.len()));
+            let v = vec!(1i);
+            assert!(is_descending_buffer(v.as_ptr(), v.len()));
+            let v : Vec<int> = vec!();
+            assert!(is_descending_buffer(v.as_ptr(), v.len()));
+            let v = vec!(1i, 2);
+            assert!(!is_descending_buffer(v.as_ptr(), v.len()));
+            let v = vec!(1i, 2, 3);
+            assert!(!is_descending_buffer(v.as_ptr(), v.len()));
+            let v = vec!(2i, 3, 1);
+            assert!(!is_descending_buffer(v.as_ptr(), v.len()));
+            let v = vec!(3i, 1, 2);
+            assert!(!is_descending_buffer(v.as_ptr(), v.len()));
+        }
     }
 
+    #[test]
+    fn test_is_ascending_buffer_strided(){
+        unsafe {
+            let v = vec!(4i, 3, 2, 1);
+            assert!(!is_ascending_buffer_strided(v.as_ptr(), v.len() / 2, 2));
+            let v = vec!(4i, 3, 5, 1);
+            assert!(is_ascending_buffer_strided(v.as_ptr(), v.len() / 2, 2));
+            let v = vec!(4i, 1, 5, 2, 3, 7);
+            assert!(!is_ascending_buffer_strided(v.as_ptr(), v.len() / 2, 2));
+            let v = vec!(4i, 3, 5, 2, 7, 1);
+            assert!(is_ascending_buffer_strided(v.as_ptr(), v.len() / 2, 2));
+        }
+    }
 
 }
 
@@ -180,7 +233,7 @@ mod bench{
     #[bench]
     fn bench_is_ascending_buffer(b: &mut Bencher){
         let v = Vec::from_fn(10000, |idx| idx * 2);        
-        b.iter(|| {
+        b.iter(|| unsafe {
             assert!(is_ascending_buffer(v.as_ptr(), v.len()));
                 });
     }
@@ -194,7 +247,7 @@ mod bench{
     #[bench]
     fn bench_is_descending_buffer(b: &mut Bencher){
         let v = Vec::from_fn(10000, |idx| -(idx as int) * 2i);        
-        b.iter(|| {
+        b.iter(|| unsafe {
             assert!(is_descending_buffer(v.as_ptr(), v.len()));
                 });
     }
